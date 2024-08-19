9ab6f67efd4d041a1f1c7a6569819f97e2390439
mohamedbassiony <mohamedbassiony333@gmail.com>
2024-08-19 23:54:20 +0300

    Create and update BaseController file to generate directory location and random string

---
+++ src/controllers/BaseController.py
@@ -0,0 +1,25 @@
+from helpers.config import get_settings, Settings
+import os
+import random
+import string
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class BaseController:
+    
+    def __init__(self):
+
+        self.app_settings = get_settings()
+        
+        self.base_dir = os.path.dirname( os.path.dirname(__file__) )
+        self.files_dir = os.path.join(
+            self.base_dir,
+            "assets/files"
+        )
+        logger.info("BaseController initialized")
+        
+    def generate_random_string(self, length: int=12):
+        return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
